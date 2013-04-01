5a7ebe4fa4f0a89a211867124d6088558390c2fb
Jeremy Muljana <jermul714@gmail.com>
2013-04-01 12:22:22 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startuptap::Application.config.secret_token = 'e4a0c1809b95cc1579582da2ac122644a13c27d20b895d1ab1f6158fe215aecd7f7c116671c29ba88154b1632d2e7f8702f471c5beaa002026898da86286cc22'
